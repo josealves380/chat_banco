63420c487aee3e933bd719667efed971f4176941
josealves380 <netopereira464@gmail.com>
2021-04-24 10:22:16 -0300

    Mudando as rotas do arquivo

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import express, { request , response } from "express";
 
 import './database';
+import { routes } from "../route";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá Cliente!",
-    });
-});
-app.post("/", (request, response) => {
-    return response.json({
-        message: "Cliente Salvo com sucesso!",
-    });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
