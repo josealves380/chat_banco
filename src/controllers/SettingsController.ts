3483704c393a2ac3eb59589d853a22f90f9fe893
josealves380 <netopereira464@gmail.com>
2021-04-24 10:25:48 -0300

    Incluindo arquivos de configuração, rotas e controller

---
+++ src/controllers/SettingsController.ts
@@ -0,0 +1,12 @@
+import {Request, Response } from 'express';
+
+
+
+class SettingsController {
+
+    async create(request: Request, response: Response) {
+
+    }
+}
+
+export { SettingsController }
